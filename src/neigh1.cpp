f30d20f890ef0157d77afe23e4eb704659187160
luiz826 <luiz.costa@academico.ufpb.br>
2022-11-24 00:37:48 -0300

    Add instances and making reinsertion. There is still some bugs in neigh1.cpp, but it is gonna work

--- src/neigh1.cpp
+++ src/neigh1.cpp
@@ -14,6 +14,48 @@
 
 using namespace std;
 
+/*
+    TODO:   Ajustar a função de checking, pois ainda dá resultados inesperados             
+*/
+
+
+bool checking(vector<vector<int>> &L, int &item, vector<int> t, vector<int> p, int Q) {
+    /*  
+        Se for possível, fazer o swap e retornar true
+        Se não for possível, retornar false
+    */ 
+
+    for (int i = 0; i < L.size(); i++) {  
+        if (L[i][0] == t[item]) {
+            for (int j = 0; j < t.size(); j++) {
+                if (t[j] == L[i][1]) {
+                    return false; // não é possível fazer o swap
+                }
+            }
+        }
+        if (L[i][1] == t[item]) {
+            for (int j = 0; j < t.size(); j++) {
+                if (t[j] == L[i][0]) {
+                    return false; // não é possível fazer o swap
+                }
+            }
+        }
+    }
+
+    int sum_w = 0;
+    for (int i=0; i < t.size(); i++) {
+        sum_w += p[t[i]];      
+    }
+
+
+    if ((sum_w + p[t[item]]) > Q) {
+        return false;
+    }
+
+
+    return true; 
+}
+
 // movimento se swap entre dois itens
 void swap(int &i1, int &i2) { 
     int temp = i1;
@@ -21,59 +63,130 @@ void swap(int &i1, int &i2) {
     i2 = temp;
 }
 
-bool checkAndSwap(vector<vector<int>> constr, int &i1, int &i2, vector<int> t1, vector<int> t2) {
-    /*  Checar os trenós de a e b e ver se é possível fazer o swap 
-        Se for possível, fazer o swap e retornar true
-        Se não for possível, retornar false
-    */ 
+void reinsertion(vector<vector<int>> &L, vector<vector<int>> &sol, int &item, int t, vector<int> p, int Q) {
+    /*  Reinsere o item no trenó com menos itens
+        Se não for possível, então o item é descartado
+    */
+
+    for (int k = 0; k < sol.size(); k++) {
+        if (k != t) {
+            if (checking(L, item, sol[k], p, Q)){
+                sol[k].push_back(sol[t][item]);
+                return ;
+            }
+        }
+    }
     
+}
+
+
+int fo(vector<vector<int>> solution) {
+    int i;
+    for (i = 0; i < solution.size(); i++); 
+
+    return i;
 }
 
 int main(void) {
 
     srand (time(NULL)); // serve para setar uma seed
     
-    int nTrenos = 3;
-    vector<int> p{4, 5, 7, 2};
-    vector<vector<int>> trenos{ { 0 }, 
-                                { 3, 1 }, 
-                                { 2 } };
-    vector<vector<int>> constr{ { 0, 1 }, 
-                                    { 0, 3 }};
+    int k = 3;
+    int Q = 10;
+    vector<int> p{3, 5, 5, 2, 1, 2};
+    vector<vector<int>> solution{{ 0, 4}, 
+                                 { 3, 1 }, 
+                                 { 2, 5 }};
+    vector<vector<int>> L{ { 0, 1 }, 
+                           { 2, 3 }};
+    
 
     /* Não criei lista de itens, utilizei os próprios indices*/
 
-
-    int chooseT1 = rand() % nTrenos; // escolhe um trenó aleatório
-    int chooseT2 = rand() % nTrenos; 
+    int chooseT1 = rand() % k; // escolhe um trenó aleatório
+    int chooseT2 = rand() % k; 
     while (chooseT1 == chooseT2) { // escolhe um trenó aleatório diferente do anterior
-        chooseT2 = rand() % nTrenos;
+        chooseT2 = rand() % k;
     }
     
-    int chooseItem1 = rand() % trenos[chooseT1].size(); // escolhe um item aleatório do trenó 1
-    int chooseItem2 = rand() % trenos[chooseT2].size(); // escolhe um item aleatório do trenó 2
+    int chooseItem1 = rand() % solution[chooseT1].size(); // escolhe um item aleatório do trenó 1
+    int chooseItem2 = rand() % solution[chooseT2].size(); // escolhe um item aleatório do trenó 2
 
-    // printa trenos antes do movimento
-    for (auto &v : trenos) { 
+    // printa solution antes do movimento   
+    cout << "Solution antes do movimento: " << endl;
+    int w;
+    for (auto &v : solution) {
+        w = 0;
         for (auto &i : v) {
-            cout << i << " - PESO: " << p[i] << endl;
+            cout << i << " ";
+            w += p[i];
         }
-        cout << endl;
+        cout << " -> " << w << endl;
     }
-    cout << endl;
+    cout << "Função objetivo: " << fo(solution) << endl;
 
-    int item1 = trenos[chooseT1][chooseItem1];
-    int item2 = trenos[chooseT2][chooseItem2];
+    if ((checking(L, chooseItem1, solution[chooseT2], p, Q)) & (checking(L, chooseItem1, solution[chooseT2], p, Q))) {
+        swap(solution[chooseT1][chooseItem1], solution[chooseT2][chooseItem2]); // faz o swap entre os itens
+    }
     
-    swap(item1, item2); // faz o swap entre os itens
 
-    cout << "======================" << endl;
-    // printa trenos depois do movimento
-    for (auto &v : trenos) {
+    // printa solution depois do movimento Swap
+    cout << "Solution depois do movimento Swap: " << endl;
+    
+    for (auto &v : solution) {
+        w = 0;
         for (auto &i : v) {
-            cout << i << " - PESO: " << p[i] << endl;
+            cout << i << " ";
+            w += p[i];
         }
-        cout << endl;
+        cout << " -> " << w << endl;
     }
+    cout << "Função objetivo: " << fo(solution) << endl;
+
+
+    /*  pegar o treno com menor peso entre chooseT1 e chooseT2
+        esvaziar o treno e tentar inserir esses itens em outros trenós
+    */ 
+
+    int wT1, wT2 = 0;
+    for (int i = 0; i < solution[chooseT1].size(); i++) {
+        wT1 += p[solution[chooseT1][i]];
+    }
+    for (int i = 0; i < solution[chooseT2].size(); i++) {
+        wT2 += p[solution[chooseT2][i]];
+    }
+    
+    
+    if (wT1 < wT2) {
+        for (int i = 0; i < solution[chooseT1].size(); i++) {
+            
+            reinsertion(L, solution, i, chooseT1, p, Q);    
+        }
+
+        solution.erase(solution.begin() + chooseT1);
+
+    } else {
+        for (int i = 0; i < solution[chooseT2].size(); i++) {
+            
+            reinsertion(L, solution, i, chooseT2, p, Q);
+        }
+
+        solution.erase(solution.begin() + chooseT2);
+    }
+
+    // printa solution depois do movimento Reinsertion
+    cout << "Solution depois do movimento Reinsertion: " << endl;
+    for (auto &v : solution) {
+        w = 0;
+        for (auto &i : v) {
+            cout << i << " ";
+            w += p[i];
+        }
+        cout << " -> " << w << endl;
+    }
+    cout << "Função objetivo: " << fo(solution) << endl;
+    
+
+    return 0;
 }
 
